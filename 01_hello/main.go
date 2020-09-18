c4e834676951aad7e6e7331686493fbfedc9838e
Will Luong <tluong@stedwards.edu>
2020-09-18 12:13:30 -0500

    Create 01_Hello
    This file contains source code to basic go program and print function. This is a Hello World file

---
+++ 01_hello/main.go
@@ -0,0 +1,10 @@
+package main
+
+// formatted package that allows us to print stuffs
+// to the screen
+import "fmt"
+
+//to run the program, run 'go run filename.go
+func main() {
+	fmt.Println("Hello World")
+}
